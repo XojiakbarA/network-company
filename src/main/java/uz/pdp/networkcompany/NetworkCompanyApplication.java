2e9811095bda1e47521b07ca62caf698933db65e
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2023-09-18 18:26:10 +0500

    init

---
+++ src/main/java/uz/pdp/networkcompany/NetworkCompanyApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.networkcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkCompanyApplication.class, args);
+    }
+
+}
